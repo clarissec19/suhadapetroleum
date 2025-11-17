1c3aa4ad36ad85d21b01956e30c6983549568e64
Clarisse Tandra <ctan0170@student.monash.edu>
2025-11-18 02:35:49 +1100

    fix random scroll issue

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/main.scss';
 import App from './App';
+import ScrollToTop from './components/ScrollToTop';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
